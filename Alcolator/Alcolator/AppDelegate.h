eabdb3bd0d8e4bfd5f1c62705051ae9ce77695af
eric beasley <ejbeasley@gmail.com>
2015-11-28 04:23:06 -0800

    checkpoint 19 completeted, Storyboarding Alcolator

---
+++ Alcolator/Alcolator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Alcolator
+//
+//  Created by eric beasley on 11/27/15.
+//  Copyright © 2015 eric j beasley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Alcolator/Alcolator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Alcolator
+//
+//  Created by eric beasley on 11/27/15.
+//  Copyright © 2015 eric j beasley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
