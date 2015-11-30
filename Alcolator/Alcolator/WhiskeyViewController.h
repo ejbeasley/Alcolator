5f3c8c85909e0f239b0aa54171e3d18decea5a8d
eric beasley <ejbeasley@gmail.com>
2015-11-30 10:05:10 -0800

    Checkpoint 20 Implementing Calculations ocmpleted

---
+++ Alcolator/Alcolator/WhiskeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhiskeyViewController.h
+//  Alcolator
+//
+//  Created by eric beasley on 11/30/15.
+//  Copyright © 2015 eric j beasley. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+@end
