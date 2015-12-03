9a2f73246b182339188cf5fd9b06af4a67dc1c3a
eric beasley <ejbeasley@gmail.com>
2015-12-03 08:59:23 -0800

    Checkpoint 21 completed

---
+++ Alcolator/Alcolator/MainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuViewController.h
+//  Alcolator
+//
+//  Created by Eric Beasley on 12/3/15.
+//  Copyright © 2015 eric j beasley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainMenuViewController : UIViewController
+
+@end
